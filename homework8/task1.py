73ae632b9cde17854ea53d8c9413764bec6a5429
Vlad Treglazov <threeeyesvl@gmail.com>
2021-08-04 13:47:35 +0300

    homework 8, correction 2

--- homework8/task1.py
+++ homework8/task1.py
@@ -13,13 +13,15 @@ class KeyValueStorage:
     def __init__(self, path_to_file: str) -> None:
         with open(path_to_file) as file:
             content = [line.strip() for line in file.readlines()]
-        kw_storage = {}
+        self.kw_storage_dict = {}
 
         for line in content:
             attr, value = line.split("=")
             value = possibly_convert_str_to_int(value)
-            kw_storage[attr] = value
-            self.kw_storage_dict = kw_storage
+            try:
+                self.kw_storage_dict[attr] = value
+            except:
+                raise ValueError("Something went wrong during the attributes assigning")
 
     def __getattr__(self, item: Any) -> Any:
         return self.kw_storage_dict[item]
