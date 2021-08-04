82ed1486edf9fcaedf51fd47e44b78f936f542cc
Vlad Treglazov <threeeyesvl@gmail.com>
2021-08-04 19:27:17 +0300

    homework 8, correction 5

--- homework8/task2.py
+++ homework8/task2.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3.dbapi2 import Cursor
 
 
 def dict_factory(cursor: sqlite3.Cursor, row: tuple) -> dict:
@@ -8,29 +9,48 @@ def dict_factory(cursor: sqlite3.Cursor, row: tuple) -> dict:
     return dictionary
 
 
+def create_cursor(func):
+    def wrapper(self, *args, **kwargs):
+        connection = sqlite3.connect(self.database_name)
+        connection.row_factory = dict_factory
+        cursor = connection.cursor()
+        try:
+            return func(self, cursor, *args, **kwargs)
+        finally:
+            cursor.close()
+            connection.close()
+
+    return wrapper
+
+
 class TableData:
     def __init__(self, database_name: str, table_name: str) -> None:
         self.database_name = database_name
         self.table_name = table_name
+
+    @create_cursor
+    def __len__(self, cursor) -> int:
+        cursor.execute(f"SELECT COUNT(*) from {self.table_name}")
+        return cursor.fetchone()["COUNT(*)"]
+
+    @create_cursor
+    def __getitem__(self, cursor, item: str) -> tuple:
+        cursor.execute(
+            f"SELECT * from {self.table_name} where name=:item", {"item": item}
+        )
+        return cursor.fetchone()
+
+    @create_cursor
+    def __contains__(self, cursor, item: str) -> bool:
+        cursor.execute(
+            f"SELECT * from {self.table_name} where name=:item", {"item": item}
+        )
+        return cursor.fetchone()
+
+    def __iter__(self):
         self.connection = sqlite3.connect(self.database_name)
         self.connection.row_factory = dict_factory
         self.cursor = self.connection.cursor()
-
-    def __len__(self) -> int:
-        self.cursor.execute(f"SELECT COUNT(*) from {self.table_name}")
-        return self.cursor.fetchone()["COUNT(*)"]
-
-    def __getitem__(self, item: str) -> tuple:
-        self.cursor.execute(
-            f"SELECT * from {self.table_name} where name=:item", {"item": item}
-        )
-        return self.cursor.fetchone()
-
-    def __contains__(self, item: str) -> bool:
-        self.cursor.execute(
-            f"SELECT * from {self.table_name} where name=:item", {"item": item}
-        )
-        return self.cursor.fetchone()
-
-    def __iter__(self):
         yield from self.cursor.execute(f"SELECT * from {self.table_name}")
+        self.cursor.close()
+        self.connection.close()
