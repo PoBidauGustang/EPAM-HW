4c6803dec27a71d3285bf3fb8b4a4ce959c556dc
Vlad Treglazov <threeeyesvl@gmail.com>
2021-08-18 17:34:00 +0300

    homework 12, correction 1

--- homework12/application/tests.py
+++ homework12/application/tests.py
@@ -1,13 +1,14 @@
-from django.test import TestCase
 import datetime
+
 from application.models import Homework
+from django.test import TestCase
 
 
 class HomeworkTestCase(TestCase):
     def setUp(self):
         Homework.objects.create(
-        text="Learn 1OOP", created=datetime.datetime.now(), deadline=5, teacher_id=1
-    )
+            text="Learn 1OOP", created=datetime.datetime.now(), deadline=5, teacher_id=1
+        )
 
     def test_homework_text(self):
         data = Homework.objects.get(deadline=5)
