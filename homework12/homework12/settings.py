a69027cb04c0627d32eff47e08a8a7e244fcfd5b
Vlad Treglazov <threeeyesvl@gmail.com>
2021-08-19 15:29:25 +0300

    homework 12, correction 22

--- homework12/homework12/settings.py
+++ homework12/homework12/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o#o+gv^6j(ol)8wnk+kldx+jtnivl=jpr^x4wunrb8ei-h@i59"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "homework12.homework12.application.apps.ApplicationConfig",
+    "homework12.application.apps.ApplicationConfig",
 ]
 
 MIDDLEWARE = [
