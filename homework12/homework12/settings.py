2b1ea06da621b0e42f9d667e565909f3d2efafec
Vlad Treglazov <threeeyesvl@gmail.com>
2021-08-19 13:14:48 +0300

    homework 12, correction 9

--- homework12/homework12/settings.py
+++ homework12/homework12/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o#o+gv^6j(ol)8wnk+kldx+jtnivl=jpr^x4wunrb8ei-h@i59"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
