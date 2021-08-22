58a961299c1429f709dfa7020b8c48e902706aa3
Vlad Treglazov <threeeyesvl@gmail.com>
2021-08-22 16:12:27 +0300

    homework12, correction 1.3

--- homework12/project/project/settings.py
+++ homework12/project/project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "orm.apps.OrmConfig",
+    "homework12.project.orm.apps.OrmConfig",
 ]
 
 MIDDLEWARE = [
